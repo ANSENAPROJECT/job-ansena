af45077ebb07aabc2a0c1b697969bba483461f1e
Fachri <fachrighiffary@gmail.com>
2021-04-26 08:32:38 +0700

    viewjob

--- src/screens/viewJob/index.js
+++ src/screens/viewJob/index.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Image} from 'react-native';
 import {StyleSheet, Text, View} from 'react-native';
+import Collapsible from 'react-native-collapsible';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import {SortBtn} from '../../assets';
 import Header from '../../components/detail/header';
+import {fonts} from '../../utils/fonts';
 
 const ViewJob = ({navigation}) => {
+  const [collapse, setCollapse] = useState(true);
   return (
     <View style={styles.container}>
       <Header navigation={navigation} />
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}>
+        <Text style={{fontFamily: fonts.SFProDisplayHeavy, fontSize: 24}}>
+          View Job Groups
+        </Text>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <TouchableOpacity
+            onPress={() => {
+              setCollapse(!collapse);
+            }}>
+            <Image source={SortBtn} style={{height: 15, width: 15}} />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image
+              source={SortBtn}
+              style={{height: 15, width: 15, marginLeft: 10}}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <Collapsible collapsed={collapse}>
+        <View
+          style={{
+            marginTop: 10,
+            height: 200,
+            backgroundColor: 'white',
+            borderRadius: 15,
+          }}></View>
+      </Collapsible>
     </View>
   );
 };
